ffcc8fa064d8163a9c2146d61179c5b3f4465555
Xande Raposo <jorgeanraposo@hotmail.com>
2021-02-18 19:00:56 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,13 @@ public class Program {
 			System.out.println(obj);	
 		}
 
+		System.out.println("\n=== TEST 3: seller findAllDepartment ===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);	
+		}
+
+		
 	}
 
 }
